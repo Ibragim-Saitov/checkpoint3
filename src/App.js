029472433cdfb550c3c460be6ba1197ff068f3fd
Ibragim-Saitov <122826573+Ibragim-Saitov@users.noreply.github.com>
2023-03-23 19:18:35 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Base from './components/Base';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Base/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Base.jsx
@@ -0,0 +1,12 @@
+
+
+function Base() {
+  return (
+    <div className="App">
+    <h1>Привет Мир</h1>
+    <img src="https://avatars.mds.yandex.net/i?id=5db6c82862bb316965b71ba565c7470a8bb47447-8261187-images-thumbs&n=13"></img>
+    </div>
+  );
+}
+
+export default Base;
